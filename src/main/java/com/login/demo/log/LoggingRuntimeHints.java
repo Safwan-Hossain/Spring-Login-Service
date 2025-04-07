8f04fae93b0950e225d169c653bf32c5078c9016
Safwan Hossain <safwanh2708@gmail.com>
2025-04-07 04:24:38 -0400

    updated logback hints for GraalVM and AOT

--- src/main/java/com/login/demo/log/LoggingRuntimeHints.java
+++ src/main/java/com/login/demo/log/LoggingRuntimeHints.java
@@ -1,5 +1,9 @@
 package com.login.demo.log;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.ConsoleAppender;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.TypeReference;
@@ -9,7 +13,9 @@ import org.springframework.context.annotation.Configuration;
 public class LoggingRuntimeHints implements RuntimeHintsRegistrar {
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-        hints.reflection().registerType(TypeReference.of("ch.qos.logback.classic.Logger"), builder -> builder.withMembers());
-        hints.reflection().registerType(TypeReference.of("ch.qos.logback.core.ConsoleAppender"), builder -> builder.withMembers());
+        hints.reflection().registerType(ConsoleAppender.class, builder -> builder.withMembers());
+        hints.reflection().registerType(PatternLayoutEncoder.class, builder -> builder.withMembers());
+        hints.reflection().registerType(LoggerContext.class, builder -> builder.withMembers());
+        hints.reflection().registerType(LoggingEvent.class, builder -> builder.withMembers());
     }
 }
